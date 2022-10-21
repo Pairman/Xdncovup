02b63d527eecdfd2611dc9fbddecd1d4cafd8fed
Null <pairmanxlr@gmail.com>
2022-10-21 16:48:45 +0800

    Update xdncovup.py

--- xdncovup.py
+++ xdncovup.py
@@ -1,21 +1,32 @@
 '''
-# Author
+# 西安电子科技大学核酸检测情况自动上报工具
+
+# 作者
 
 [Pairman](https://github.com/Pairman)
 
-# Disclaimer
+# 免责信息
 
 本程序仅供学习交流使用，使用本程序造成的任何后果由用户自行负责。
 
-# Dependencies
+# 依赖
 
 ```Python>=3``` , ```requests```
 
-# Usage
+# 用法
 
-将本程序( ```.py``` )下载到本地，在用户配置修改区域填入自己的学号、密码及上报地址，保持运行本程序，本程序将自动在相应时段上报核酸检测情况。
+```
+用法：
+    python3 xdncovup.py [参数]
+参数：
+    -h,--help                   输出帮助信息
+    -u,--username <学号>        指定学号
+    -p,--password <密码>        指定密码
+    -l,--location <上报地址>    指定上报地址（格式：某国某省某市某县/区）
+    -d,--debug                  进入调试模式
+```
 
-# Credits
+# 致谢
 
 [使用Github Aciton自动填写疫情通](https://cnblogs.com/soowin/p/13461451.html)
 
@@ -25,33 +36,63 @@
 
 [西安电子科技大学晨午晚检自动上报工具](https://github.com/cunzao/ncov)
 
-# Lisense
+# 开源协议
 
 GNU General Public License v3.0 (gpl-3.0)
-
 '''
 
-# ------------------------------------------------ #
-
-# 用户配置修改区域
-
-# 登录学号和密码，在引号内修改
-USERNAME=""
-PASSWORD=""
-
-# 上报地址，如某国某省某市某县/区，在引号内修改
-LOCATION=""
-
-# 调试用，不懂勿动
-NOTDEBUG=True
-
-# ------------------------------------------------ #
-
 from asyncio.windows_events import NULL
-import datetime
-import random
-import requests
-import time
+from datetime import datetime
+from getopt import getopt
+from random import randint
+from requests import Session
+from sys import argv
+from time import sleep
+
+opts=getopt(argv[1:],"hu:p:l:d",["help","username=","password=","location=","debug"])[0]
+
+USERNAME,PASSWORD,LOCATION,DEBUG="","","",False
+
+helpMsg="""Xdncovup - 西安电子科技大学核酸检测情况自动上报工具 1.1 (2022 Oct 21, Pairman)
+本程序仅供学习交流使用，使用本程序造成的任何后果由用户自行负责。
+用法：
+    python3 %s [参数]
+参数：
+    -h,--help                   输出帮助信息
+    -u,--username <学号>        指定学号
+    -p,--password <密码>        指定密码
+    -l,--location <上报地址>    指定上报地址（格式：某国某省某市某县/区）
+    -d,--debug                  进入调试模式
+"""%(argv[0])
+
+if len(argv)==1:
+    print(helpMsg)
+    exit()
+
+for opt,arg in opts:
+    if opt in ("-h","--help"):
+        print(helpMsg)
+        exit()
+    if opt in ("-u","--username"):
+        USERNAME=arg
+    if opt in ("-p","--password"):
+        PASSWORD=arg
+    if opt in ("-l","--location"):
+        LOCATION=arg
+    if opt in ("-d","--debug"):
+        DEBUG=1
+
+print("本程序仅供学习交流使用，使用本程序造成的任何后果由用户自行负责。")
+
+if USERNAME=="":
+    print("请指定学号！")
+    exit()
+if PASSWORD=="":
+    print("请指定密码！")
+    exit()
+if LOCATION=="":
+    print("请指定上报地址！")
+    exit()
 
 # 上报信息表
 currentUploadMsg={
@@ -64,15 +105,13 @@ currentUploadMsg={
 # 默认上报时间
 upHour,upMinute=8,30
 
-print("核酸检测情况自动上报")
-
 # 登录
-conn=requests.Session()
+conn=Session()
 logined=0
 for i in range(3):
     result=NULL
-    try :
-        result=conn.post(url="https://xxcapp.xidian.edu.cn/uc/wap/login/check",data={"username":USERNAME,"password":PASSWORD},verify=NOTDEBUG)
+    try:
+        result=conn.post(url="https://xxcapp.xidian.edu.cn/uc/wap/login/check",data={"username":USERNAME,"password":PASSWORD},verify=not DEBUG)
         if result.json()['e']==0:
             logined=1
             print("登录成功")
@@ -89,9 +128,9 @@ def ncovUp():
     result=NULL
     for i in range(3):
         try:
-            result=conn.post(url="https://xxcapp.xidian.edu.cn/forms/wap/default/get-info?formid=563",verify=NOTDEBUG)
+            result=conn.post(url="https://xxcapp.xidian.edu.cn/forms/wap/default/get-info?formid=563",verify=not DEBUG)
             currentUploadMsg["value[id]"]=result.json()['d']['value']['id']
-            result=conn.post(url="https://xxcapp.xidian.edu.cn/forms/wap/default/save",data=currentUploadMsg,verify=NOTDEBUG)
+            result=conn.post(url="https://xxcapp.xidian.edu.cn/forms/wap/default/save",data=currentUploadMsg,verify=not DEBUG)
             if result.json()['e']==0:
                 print("上报成功")
                 return 1
@@ -108,20 +147,20 @@ def ncovUp():
 ncovUp()
 
 while True:
-    currentTime=datetime.datetime.now()
+    currentTime=datetime.now()
     currentHour,currentMinute=int(str(currentTime)[11:13]),int(str(currentTime)[14:16])
     # 到上报时间时尝试上报
     if currentHour==upHour and currentMinute==upMinute:
-        currentUploadMsg["value[date_563_2]"]=str(datetime.datetime.now())[0:10]
+        currentUploadMsg["value[date_563_2]"]=str(datetime.now())[0:10]
         ncovUp()
     # 其他时刻暂停上报
     elif currentHour<upHour or (currentHour==upHour and currentMinute<upMinute-10):
-        time.sleep(29340-3600*currentHour-60*currentMinute)
+        sleep(29340-3600*currentHour-60*currentMinute)
     elif currentHour>upHour or (currentHour==upHour and currentMinute>upMinute):
-        upMinute=random.randint(10,50)
+        upMinute=randint(10,50)
         print("更新核酸检测情况上报时间成功！下一天上报的时间为:%02d时%02d分"%(upHour,upMinute))
         print("程序进入休眠")
         # halt till 8:09 next day
-        time.sleep(86100+3600*upHour-3600*currentHour+60*upMinute-60*currentMinute)
+        sleep(86100+3600*upHour-3600*currentHour+60*upMinute-60*currentMinute)
     else:
-        time.sleep(60)
+        sleep(60)
